a07f2573dff564ec5bf3299648648b06cdfe9e1c
SolanoD94 <dianasolano4970@gmail.com>
2023-05-21 18:30:35 -0600

    download cv button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ export function Button() {
   return (
     <div className="mt-10 flex items-center justify-center gap-x-6 ">
       <a
-        href="https://github.com/SolanoD94"
+        href="https://drive.google.com/file/d/1a9663PI34tTms8o_edsjtv_9gdLXIB2s/view?usp=sharing"
         target="_blank"
         rel="noreferrer"
         className="text-center rounded-md w-full bg-zinc-200 px-3.5 py-2.5 text-sm font-medium text-zinc-900 shadow-sm hover:bg-zinc-200/70 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300"
